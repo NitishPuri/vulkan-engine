e97e4e5407f37ccae2b8d7db612f94d8854011fa
Nitish Puri <npuri1903@gmail.com>
2023-01-28 15:20:39 +0530

    Use tinyobjloader to load obj files. Load vertex data from obj files.

--- VulkanEngine/lve_model.h
+++ VulkanEngine/lve_model.h
@@ -6,21 +6,34 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 
+#include <memory>
+
 namespace lve {
 	class LveModel {
 	public:
 
 		struct Vertex {
-			glm::vec3 position;
-			glm::vec3 color;
+			glm::vec3 position{};
+			glm::vec3 color{};
+			glm::vec3 normal{};
+			glm::vec2 uv{};
 
 			static std::vector<VkVertexInputBindingDescription> getBindingDescriptions();
 			static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
+
+			bool operator==(const Vertex& other) const {
+				return position == other.position && color == other.color
+					&& normal == other.normal && uv == other.uv;
+			}
+
+
 		};
 
 		struct Builder {
 			std::vector<Vertex> vertices{};
 			std::vector<uint32_t> indices{};
+
+			void loadModel(const std::string& filepath);
 		};
 
 		LveModel(LveDevice& device, const Builder &builder);
@@ -28,6 +41,8 @@ namespace lve {
 
 		LveModel(const LveModel&) = delete;
 		LveModel operator=(const LveModel&) = delete;
+
+		static std::unique_ptr<LveModel> createModelFromFile(LveDevice& device, const std::string& filepath);
 
 		void bind(VkCommandBuffer commandBuffer);
 		void draw(VkCommandBuffer commandBuffer);
